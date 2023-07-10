8dda3f2401a60dc5af2e786e226f48624c6434aa
Suriyanaath <100365292+Suriyanaath@users.noreply.github.com>
2023-07-10 23:45:02 +0530

    Add files via upload

---
+++ SQL_Basics.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select*from customer
+LIMIT 0, 1000
+
+-- Date: 2023-07-10 23:28
+*/
+INSERT INTO `` (`customer_accNo`,`customer_pin`,`customer_name`,`balance_amount`,`acc_type`,`customer_panNo`,`cus_contactNo`,`cus_address`,`branch_id`) VALUES (2022120,7899,'Kaviya',150000,'Savings_account','kvy183','9842005874','Erode','ED15');
+INSERT INTO `` (`customer_accNo`,`customer_pin`,`customer_name`,`balance_amount`,`acc_type`,`customer_panNo`,`cus_contactNo`,`cus_address`,`branch_id`) VALUES (2020110,1599,'Suriya',52354,'Current_account','MGS23','9087654329','Madurai','MDU14');
+INSERT INTO `` (`customer_accNo`,`customer_pin`,`customer_name`,`balance_amount`,`acc_type`,`customer_panNo`,`cus_contactNo`,`cus_address`,`branch_id`) VALUES (2018119,4711,'Nisha',50000,'Savings_account','Nishy147','9788647815','Salem','SLM21');
+INSERT INTO `` (`customer_accNo`,`customer_pin`,`customer_name`,`balance_amount`,`acc_type`,`customer_panNo`,`cus_contactNo`,`cus_address`,`branch_id`) VALUES (2016011,3569,'Navaneedhan',44587,'Current_account','NVN146','7540085474','Attur','ATT02');
+INSERT INTO `` (`customer_accNo`,`customer_pin`,`customer_name`,`balance_amount`,`acc_type`,`customer_panNo`,`cus_contactNo`,`cus_address`,`branch_id`) VALUES (2020101,3577,'Rashik',25680,'Savings_account','Rash123','9845200114','Rasipuram','RSP01');
